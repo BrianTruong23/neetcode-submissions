7d75acfc86fabf6feab2152df566067dd4c81748
Thang Truong <40693511+BrianTruong23@users.noreply.github.com>
2026-06-30 09:23:55 -0500

    Add: balanced-binary-tree - submission-1

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-1.py
@@ -0,0 +1,42 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        # get the height for left and right 
+        # if height for left is larger than right or right larger by left more than 1 then return false 
+        # apply dfs to this and determine the height for right and left at each node
+        # if right > left + 1 or left > right + 1 then return False 
+        # so this function return True or False but also we keep track of height for left and right 
+        # height
+        # how about we have nonlocal variable to keep track true or false 
+        balanced = 0
+
+        def dfs(node):
+            
+            nonlocal balanced 
+            # go to left and right 
+
+            if not node:
+                return 0 
+            
+            left = dfs(node.left)
+            right = dfs(node.right)
+
+
+            if left > right + 1 or right > left + 1:
+                balanced += 1
+            
+            return max(left, right) + 1 
+
+        dfs(root)
+
+        if balanced != 0:
+            return False 
+        
+        return True 
+        
