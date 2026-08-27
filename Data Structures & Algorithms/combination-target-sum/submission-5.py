b7e9e3f55b3672c4add96811f192a199773dd3d4
Thang Truong <40693511+BrianTruong23@users.noreply.github.com>
2026-08-27 09:58:59 -0500

    Add: combination-target-sum - submission-5

---
+++ Data Structures & Algorithms/combination-target-sum/submission-5.py
@@ -0,0 +1,38 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        
+
+        # sort 
+        nums.sort()
+        # result array 
+        result = []
+
+        # write dfs with backtracking 
+
+        def dfs(index: int, remaining, path: List[int]):
+            
+            if remaining == 0:
+                result.append(path[:])
+                return
+
+            if remaining < 0:
+                return 
+            
+            # print(path, remaining)
+            for i in range(index, len(nums)):
+
+                if nums[i] > remaining: 
+                    break
+                
+                path.append(nums[i])
+
+                dfs(i, remaining - nums[i], path)
+
+                path.pop()
+      
+        
+        dfs(0, target, [])
+
+        return result 
+
+                
