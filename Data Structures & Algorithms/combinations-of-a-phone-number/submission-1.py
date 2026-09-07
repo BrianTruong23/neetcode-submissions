567802df67340612234693b36ea9335a2b3ee3df
Thang Truong <40693511+BrianTruong23@users.noreply.github.com>
2026-09-07 10:11:46 -0500

    Add: combinations-of-a-phone-number - submission-1

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-1.py
@@ -0,0 +1,29 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        
+        # permutations 
+        # 345
+        # def, ghi, jkl
+        # d g j, d g k, d g l 
+
+        
+        maps = {"2": ['a', 'b', 'c'], "3": ['d', 'e', 'f'], "4": ['g', 'h', 'i'], "5": ['j', 'k', 'l'], "6": ['m', 'n', 'o'], "7": ['p', 'q', 'r', 's'], "8": ['t', 'u', 'v'], "9": ['w', 'x', 'y', 'z']}
+
+        result = []
+
+        if len(digits) == 0:
+            return []
+        def dfs(index, path):
+            
+            # base case 
+            if index == len(digits):
+                result.append(path)
+                return 
+            
+            digit_current = digits[index]
+            for char in maps[digit_current]:
+                dfs(index + 1, path + char)
+  
+        
+        dfs(0, '')
+        return result
